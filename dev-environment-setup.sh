d0d9aad5ceac129f5ba5c6a2931025181598b849
mark_rees <mark.rees@birdfood.co.uk>
2021-12-28 19:50:31 +0000

    updates to main dev script

--- dev-environment-setup.sh
+++ dev-environment-setup.sh
@@ -1,21 +1,27 @@
-different_user="mark"
-home_user=$(bash -c "cd ~$(printf %q $different_user) && pwd")
+#!/bin/sh
 
+# common packages
 sudo apt-get install apt-transport-https software-properties-common dirmngr 
 
 # php
-sudo apt install php7.4 php7.4-pdo php7.4-curl php7.4-mysqlnd php7.4-opcache php7.4-xml php7.4-gd php7.4-mysql php7.4-intl php7.4-mbstring php7.4-bcmath php7.4-json php7.4-iconv php7.4-soap php-dev
+sudo apt install php7.4 php7.4-pdo php7.4-curl php7.4-mysqlnd php7.4-opcache php7.4-xml php7.4-gd php7.4-mysql php7.4-intl php7.4-mbstring php7.4-bcmath php7.4-json php7.4-iconv php7.4-soap php7.4-zip php-dev
+
+# git & unzip
+sudo apt install unzip git
 
 # composer
-sh $home_user/Documents/setup-scripts/composer-setup.sh
+if [ ! -f "/tmp/composer-setup.sh" ]
+then
+    wget -O /tmp/composer-setup.sh https://raw.githubusercontent.com/Sental/setup-dev-environment/main/composer-setup.sh
+fi
+sh /tmp/composer-setup.sh
 
 # vscode
-# sudo dpkg -i $home_user/Documents/setup-scripts/code_1.63.2-1639562499_amd64.deb
-# sudo apt-get install -f # Install dependencies
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
+sudo apt update && sudo apt install code
 
 # elasticsearch
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
@@ -30,8 +36,12 @@ sudo apt install redis
 # varnish
 sudo apt install varnish
 
-# rabbitmq
-sh $home_user/Documents/setup-scripts/rabbit-mq.sh
+# rabbit mq
+if [ ! -f "/tmp/rabbit-mq.sh" ]
+then
+    wget -O /tmp/rabbit-mq.sh https://raw.githubusercontent.com/Sental/setup-dev-environment/main/rabbit-mq.sh
+fi
+sh /tmp/rabbit-mq.sh
 
 # mariadb
 sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
@@ -39,4 +49,6 @@ sudo add-apt-repository 'deb [arch=amd64,i386,arm64,ppc64el] https://mirrors.xto
 sudo apt-get update
 sudo apt-get install mariadb-server
 
-sudo apt install unzip git
+echo "starting interactive mariadb configuration from the mariadb package's script"
+/bin/sleep 3s
+mariadb-install-db
